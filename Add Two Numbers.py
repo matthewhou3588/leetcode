11708353da5616a88871b2050509b330095c6769
matthewhou3588 <hyfzxs@163.com>
2020-01-13 22:06:31 +0800

    Update Add Two Numbers.py

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -1,5 +1,5 @@
 #https://leetcode.com/problems/add-two-numbers/
-
+'''
 You are given two non-empty linked lists representing two non-negative integers. 
 The digits are stored in reverse order and each of their nodes contain a single digit. 
 Add the two numbers and return it as a linked list.
@@ -12,8 +12,9 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 
+'''
 
-Answer:
+# Answer:
 
 # Definition for singly-linked list.
 # class ListNode(object):
